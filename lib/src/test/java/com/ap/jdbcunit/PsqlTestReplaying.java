4625d9500b9a63b303cab437a83c5748d33578a4
Victor Tatai <vtatai@google.com>
2023-12-15 16:43:57 +0000

    Fixing test
    Change-Id: I11f500d43bfc43c3502925b2b5cb866d3f877da5

--- lib/src/test/java/com/ap/jdbcunit/PsqlTestReplaying.java
+++ lib/src/test/java/com/ap/jdbcunit/PsqlTestReplaying.java
@@ -11,13 +11,16 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import org.junit.Test;
 
 /**
  * Just used for manual testing for now.
  */
 public class PsqlTestReplaying {
+    @Test
     public void testSelect() throws Exception {
         File csv = new File("test-data/test-data.csv");
         Store store = new JavaFile(csv);
@@ -34,6 +37,16 @@ public class PsqlTestReplaying {
         props.setProperty("password", "jdbcunit");
         props.setProperty("ssl", "false");
 
+        query(url, props);
+
+        JDBCUnit.replay();
+
+        query(url, props);
+
+        JDBCUnit.stop();
+    }
+
+    private static void query(String url, Properties props) throws SQLException {
         Connection con = DriverManager.getConnection(url, props);
         Statement stmt = con.createStatement();
         ResultSet rs = stmt.executeQuery("SELECT * FROM users");
@@ -41,20 +54,8 @@ public class PsqlTestReplaying {
         while (rs.next()) {
             System.out.printf("id %d name %s%n", rs.getInt(1), rs.getString(2));
         }
-
         rs.close();
         stmt.close();
-
-        JDBCUnit.replay();
-
-        stmt = con.createStatement();
-        rs = stmt.executeQuery("SELECT * FROM users");
-
-        while (rs.next()) {
-            System.out.printf("id %d name %s%n", rs.getInt(1), rs.getString(2));
-        }
-
-        JDBCUnit.stop();
     }
 
     public static void main(String[] args) throws Exception {
